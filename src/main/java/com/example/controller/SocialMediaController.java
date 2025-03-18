c10cb18bae4cfb9631768a6d752ab018d7721a09
Robert Boyette <boyetterobert5@gmail.com>
2025-03-18 11:27:17 +0000

    Completed Spring Social Media API Project

--- src/main/java/com/example/controller/SocialMediaController.java
+++ src/main/java/com/example/controller/SocialMediaController.java
@@ -5,7 +5,6 @@ import com.example.entity.Message;
 import com.example.service.AccountService;
 import com.example.service.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
